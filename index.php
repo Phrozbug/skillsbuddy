125ebd4c578329c9181793c7dc20721beaa3badf
Phrozbug <marcroelofs1970@gmail.com>
2022-02-04 10:36:44 +0100

    Meta names en title aangepast in index.php

--- index.php
+++ index.php
@@ -29,7 +29,12 @@ $inhoud_diversen = $pdo->query('SELECT * FROM skills WHERE taal = "Diversen"');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style.css" />
-    <title>Skillsbuddy - Eindopdracht Marc Roelofs</title>
+    <title>Skillsbuddy - Notities voor de Web Developer</title>
+    <meta name="description" content="Met Skillsbuddy kun je bijhouden welke programmeertalen je hebt geleerd. Per taal kun je de relevante informatie raadplegen, veranderen en toevoegen."/>
+    <meta name="keywords" content="full stack web development, coding, programmeren, notities, html, css, MySQL, PHP, PDO, PHP-PDO, javascript, python, git"/>
+    <meta name="author" content="Marc Roelofs" />
+    <meta name="copyright" content="Marc Roelofs" />
+
 </head>
 
 <body>
